1e47502d79be9d60c2b55cee97ac8cdf5f53eedd
Zach Plunkett <Zach13plunkett@gmail.com>
2020-10-20 19:51:37 -0400

    completed triangle

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,9 +1,12 @@
 // Package triangle provides a triangle type finding function
 package triangle
 
-// Type kind is either a triangle type or not a triangle
+import "math"
+
+// Kind is either a triangle type or not a triangle
 type Kind string
 
+// Constants for types of triangles as well as not a triangle
 const (
 	NaT Kind = "NaT" // not a triangle
 	Equ Kind = "Equ" // equilateral
@@ -11,60 +14,29 @@ const (
 	Sca Kind = "Sca" // scalene
 )
 
-// KindFromSides should have a comment documenting it.
-func KindFromSides(a, b, c float64) Kind {
-	return NaT
+// realNumber determines if three given numbers are realnumbers
+func realNumber(a, b, c float64) bool {
+	nums := []float64{a, b, c}
+
+	for _, i := range nums {
+		if math.IsNaN(i) || math.IsInf(i, 0) {
+			return false
+		}
+	}
+	return true
 }
 
-/*
-# Triangle
+// KindFromSides should have a comment documenting it.
+func KindFromSides(a, b, c float64) Kind {
 
-Determine if a triangle is equilateral, isosceles, or scalene.
-
-An _equilateral_ triangle has all three sides the same length.
-
-An _isosceles_ triangle has at least two sides the same length. (It is sometimes
-specified as having exactly two sides the same length, but for the purposes of
-this exercise we'll say at least two.)
-
-A _scalene_ triangle has all sides of different lengths.
-
-## Note
-
-For a shape to be a triangle at all, all sides have to be of length > 0, and
-the sum of the lengths of any two sides must be greater than or equal to the
-length of the third side. See [Triangle Inequality](https://en.wikipedia.org/wiki/Triangle_inequality).
-
-## Dig Deeper
-
-The case where the sum of the lengths of two sides _equals_ that of the
-third is known as a _degenerate_ triangle - it has zero area and looks like
-a single line. Feel free to add your own code/tests to check for degenerate triangles.
-
-## Constant Declarations
-
-In this exercise you are asked to declare a series of constants used to identify
-different types of triangles. Pick the data type you think works best for this
-and get the tests passing.
-
-Afterwards, it may be worth reading about Go's
-predeclared identifier [iota](https://golang.org/ref/spec#Iota). There's an
-excellent write up about it
-[here](https://splice.com/blog/iota-elegant-constants-golang/).
-
-
-## Coding the solution
-
-Look for a stub file having the name triangle.go
-and place your solution code in that file.
-
-## Running the tests
-
-To run the tests run the command `go test` from within the exercise directory.
-
-If the test suite contains benchmarks, you can run these with the `--bench` and `--benchmem`
-flags:
-
-    go test -v --bench . --benchmem
-
-*/
+	// Not a Triangle
+	if !(a > 0 && b > 0 && c > 0 && a+b >= c && a+c >= b && b+c >= a) ||
+		!realNumber(a, b, c) {
+		return NaT
+	} else if a == b && b == c {
+		return Equ
+	} else if a == b || b == c || c == a {
+		return Iso
+	}
+	return Sca
+}
